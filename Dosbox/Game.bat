103ec368a146bcd53afa0127cf2d54f5fce8ac8b
Kangrok Kim <kimpro@uebermensch.tech>
2021-08-20 07:25:18 +0900

    Replace Images/Dosbox_game.PNG

--- Dosbox/Game.bat
+++ Dosbox/Game.bat
@@ -10,7 +10,7 @@ echo 1. Princess Maker 2
 echo 2. Mother Goose
 echo 3. Park
 set /p x=Enter your choice(1-3) : 
-if "%x%"=="1" echo c:\game\pm2\pm2\exe
+if "%x%"=="1" echo c:\game\pm2\pm2.exe
 if "%x%"=="2" echo c:\game\mg\mg.exe
 if "%x%"=="3" echo c:\park.exe
 goto LIST
