c8f176de56b7d026838d8a976a713b603b96e25f
Kangrok Kim <kimpro@uebermensch.tech>
2021-12-03 01:25:48 +0900

    Update AlphaCa2.r

--- AlphaCa/AlphaCa2.r
+++ AlphaCa/AlphaCa2.r
@@ -6,18 +6,18 @@
 
 # 2-1. Case that has two lines of odd sum
 
-k=86532; a.arrow[,,k]													# Winner : 1st (5-3-7 for the '/' line, not 9-3-1 on the 2nd column)
+k=86532; a.arrow[,,k]                                                   # Winner : 1st (5-3-7 for the '/' line, not 9-3-1 on the 2nd column)
 
 
 # 2-1-1. Get sums of all the lines
 
-wl <- c()																# wl(win/lose) : 0 (2nd player wins) / 1~2 (draw) / 3 (1st one wins)
+wl <- c()                                                               # wl(win/lose) : 0 (2nd player wins) / 1~2 (draw) / 3 (1st one wins)
 for (i in 1:3) {
-	wl <- c(wl, sum(a.arrow[,i,k]%%2))
-	}
+    wl <- c(wl, sum(a.arrow[,i,k]%%2))
+    }
 for (i in 1:3) {
-	wl <- c(wl, sum(a.arrow[i,,k]%%2))
-	}
+    wl <- c(wl, sum(a.arrow[i,,k]%%2))
+    }
 wl <- c(wl, sum(diag(a.arrow[,,k])%%2))
 wl <- c(wl, sum(c(a.arrow[1,3,k],a.arrow[2,2,k],a.arrow[3,1,k])%%2))
 wl; mm <- c(max(wl), min(wl)); mm
@@ -25,15 +25,15 @@ wl; mm <- c(max(wl), min(wl)); mm
 
 # 2-1-2. Improved code : add wl.max
 
-wl <- c(); wl.max <- c()												# wl.max : the max number of each line
+wl <- c(); wl.max <- c()                                                # wl.max : the max number of each line
 for (i in 1:3) {
-	wl <- c(wl, sum(a.arrow[,i,k]%%2))
-	wl.max <- c(wl.max, max(a.arrow[,i,k]))
-	}
+    wl <- c(wl, sum(a.arrow[,i,k]%%2))
+    wl.max <- c(wl.max, max(a.arrow[,i,k]))
+    }
 for (i in 1:3) {
-	wl <- c(wl, sum(a.arrow[i,,k]%%2))
-	wl.max <- c(wl.max, max(a.arrow[i,,k]))
-	}
+    wl <- c(wl, sum(a.arrow[i,,k]%%2))
+    wl.max <- c(wl.max, max(a.arrow[i,,k]))
+    }
 wl <- c(wl, sum(diag(a.arrow[,,k])%%2))
 wl.max <- c(wl.max, max(diag(a.arrow[,,k])))
 wl <- c(wl, sum(c(a.arrow[1,3,k],a.arrow[2,2,k],a.arrow[3,1,k])%%2))
@@ -56,13 +56,13 @@ k=86537; a.arrow[,,k]
 
 wl <- c(); wl.max <- c()
 for (i in 1:3) {
-	wl <- c(wl, sum(a.arrow[,i,k]%%2))
-	wl.max <- c(wl.max, max(a.arrow[,i,k]))
-	}
+    wl <- c(wl, sum(a.arrow[,i,k]%%2))
+    wl.max <- c(wl.max, max(a.arrow[,i,k]))
+    }
 for (i in 1:3) {
-	wl <- c(wl, sum(a.arrow[i,,k]%%2))
-	wl.max <- c(wl.max, max(a.arrow[i,,k]))
-	}
+    wl <- c(wl, sum(a.arrow[i,,k]%%2))
+    wl.max <- c(wl.max, max(a.arrow[i,,k]))
+    }
 wl <- c(wl, sum(diag(a.arrow[,,k])%%2))
 wl.max <- c(wl.max, max(diag(a.arrow[,,k])))
 wl <- c(wl, sum(c(a.arrow[1,3,k],a.arrow[2,2,k],a.arrow[3,1,k])%%2))
