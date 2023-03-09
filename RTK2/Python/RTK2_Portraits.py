6ad5063c50881854c906313f596fee9e8d7d8c75
Kangrok Kim <kimpro@uebermensch.tech>
2023-03-10 03:22:44 +0900

    Update RTK2_Portraits.py (not completed)

--- RTK2/Python/RTK2_Portraits.py
+++ RTK2/Python/RTK2_Portraits.py
@@ -1,50 +1,77 @@
 # RTK2 : Get Portraits from KAODATA.DAT
-# 2023.03.07
+
+# 2023.03.09
+
 
 import os
 from PIL import Image
 
+
+# Parameters
 test = True                                                 # True : Test Mode
-
-
-# 0. Read the file if exists
-
 path = "C:\Game\KOEI\RTK2\KAODATA.DAT"
-if (os.path.isfile(path)):
-    with open(path, 'rb') as f:
-        data = f.read()
+palette = [
+    (0, 0, 0),        # Black
+    (255, 255, 255),  # White
+    (255, 0, 0),      # Red
+    (0, 255, 0),      # Green
+    (0, 0, 255),      # Blue
+    (255, 255, 0),    # Yellow
+    (255, 0, 255),    # Magenta
+    (0, 255, 255),    # Cyan
+]
+
+
+def ReadPath(path):
+    if (os.path.isfile(path)):
+        with open(path, "rb") as f:
+            data = f.read()
+            if test:
+                print("test : ", data[0], type(data[0]), bin(data[0]))  # OK : 0 85 <class 'int'> 0b1010101 ……
+            return data
+    else:
+        print("There's no target file.")
+        exit()
+
+
+def Extract3Bits(data):
+    pixels = []
+    for byte in data:
+        for i in range(8):  # iterate over 8 bits (1 byte)
+            # extract 3-bit data
+            pixel_value = (byte >> (3*i)) & 0b111  
+            pixels.append(pixel_value)
     if test:
-        print(data[:10])                                    # OK
-else:
-    print("There's no target file.")
-    exit()
+        print("pixels : ", pixels[:5])
+    return pixels
 
 
-# 1. Slicing the data
+def ConvertColors(pixels):
+    image_data = [palette[pixel_value] for pixel_value in pixels]
+    if test:
+        print("converted colors : ", image_data[:5])
+    return image_data
 
 
+def SaveImage(image_data):
+    width = 64
+    height = int(len(image_data) / width)
+    im = Image.new("RGB", (width, height))
+    im.putdata(image_data)
+    im.save("./Images/RTK2_Portraits.gif", "GIF")
 
-chunks = [data[i:i+3] for i in range(0, 64 * 80, 3)]
-# chunks = [data[i:i+3] for i in range(0, len(data), 3)]
-if test:
-    print(chunks[:5])                                       # OK
-    print(int(chunks[0]))
 
+# Run
+if __name__ == "__main__":
 
-# 2. Get portraits
+    # 1. Read data or do exit() if not exists
+    data = ReadPath(path)
 
-# Each general's portrait consists of (64, 80) size
+    # 2. Extract data in 3-bit chunks
+    pixels = Extract3Bits(data)
 
-# gif_frames = []
-# for chunk in chunks:
-#     # 각 바이트를 픽셀값으로 변환
-#     pixels = [int(b) for b in chunk]
-#     print(pixels[:5])
-#     # 이미지 생성
-#     img = Image.new('1', (3, 1), 0)
-#     img.putdata(pixels)
-#     # GIF 프레임 추가
-#     gif_frames.append(img)
+    # 3. Convert each pixel value to a color from the palette
+    image_data = ConvertColors(pixels)
 
-# # # GIF 파일 저장
-# gif_frames[0].save('./images/RTK2_Portraits.gif', format='GIF', append_images=gif_frames[1:], save_all=True, duration=100, loop=0)
+    # 4. Save into a gif file
+    SaveImage(image_data)
