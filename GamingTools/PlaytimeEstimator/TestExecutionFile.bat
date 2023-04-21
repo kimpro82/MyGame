79f96338aec9605470e003a934ea7ab4074346a1
Kangrok Kim <kimpro@uebermensch.tech>
2023-04-22 02:27:30 +0900

    Update TestExecutionFile.bat

--- GamingTools/PlaytimeEstimator/TestExecutionFile.bat
+++ GamingTools/PlaytimeEstimator/TestExecutionFile.bat
@@ -12,11 +12,11 @@ set end_time=!time!
 @REM set /a "start_ms=((1!start_time:~0,2!-100)*3600 + (1!start_time:~3,2!-100)*60 + (1!start_time:~6,2!-100))*100 + (1!start_time:~9,2!-100)"
 @REM set /a "end_ms=((1!end_time:~0,2!-100)*3600 + (1!end_time:~3,2!-100)*60 + (1!end_time:~6,2!-100))*100 + (1!end_time:~9,2!-100)"
 @REM set /a "elapsed_time=(end_ms - start_ms) / 100"
-@REM ±ÕÇüÀÌ ¸ÂÁö ¾Ê´Â °ıÈ£
+@REM ê· í˜•ì´ ë§ì§€ ì•ŠëŠ” ê´„í˜¸
 
-echo [½ÇÇàÆÄÀÏ Ãâ·Â °³½Ã]
-echo  ½ÇÇà½Ã°£ : %start_time% ~ %end_time%
-@REM echo  ½ÇÇà½Ã°£ : %elapsed_time%ÃÊ (%start_time% ~ %end_time% )
-echo [½ÇÇàÆÄÀÏ Ãâ·Â Á¾·á]
+echo [%0 starts running.]
+echo  Running time : %start_time% ~ %end_time%
+@REM echo  Running time : %elapsed_time%?? (%start_time% ~ %end_time% )
+echo [%0 has ended.]
 
 endlocal
